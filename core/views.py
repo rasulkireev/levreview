f15b0f6162a6318edfac6b0eea7c245381e9c077
Rasul Kireev <rasul.kireev@guycarp.com>
2022-10-30 15:19:15 -0400

    not doing update view for now

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, CreateView, DetailView
+from django.views.generic import CreateView, DetailView
 from django.urls import reverse
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
