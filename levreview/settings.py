b297b789a1826cd4a2d7742900c5122733548173
Rasul Kireev <rasul.kireev@guycarp.com>
2022-11-02 17:00:26 -0400

    static update

--- levreview/settings.py
+++ levreview/settings.py
@@ -136,14 +136,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
-    BASE_DIR.joinpath("frontend/build"),
+    os.path.join(BASE_DIR, "frontend/build"),
 ]
 
 WEBPACK_LOADER = {
-    "MANIFEST_FILE": BASE_DIR.joinpath("frontend/build/manifest.json"),
+    "MANIFEST_FILE": os.path.join(BASE_DIR, "frontend/build/manifest.json"),
 }
 
 # Default primary key field type
