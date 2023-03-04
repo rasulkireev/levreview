170ae9ed3ac9fda1152ff891bdb9c4bd6ef83584
Rasul Kireev <rasul.kireev@guycarp.com>
2023-03-04 01:11:57 -0500

    use manual port

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -2,4 +2,4 @@ RUN python manage.py collectstatic --noinput
 python manage.py migrate
 # python manage.py djstripe_sync_models
 
-gunicorn --bind 0.0.0.0:$APP_PORT --workers 3 levreview.wsgi:application
+gunicorn --bind 0.0.0.0:80 --workers 3 levreview.wsgi:application
